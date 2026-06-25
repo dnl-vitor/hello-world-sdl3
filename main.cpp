bc2d0ab0de35a2a9870866be92f6e9aca03ea08e
dnl-vitor <danilohvitor@gmail.com>
2026-06-25 04:00:18 -0300

    adicionando interações à janela(copiar), e centralizando o texto

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <SDL3/SDL.h>
 #include <cstdlib>
+#include <cstring>
 
 int main(int args, char *argv[]){
-
+    
     //inicialização do subsistema de vídeo
     if(!SDL_Init(SDL_INIT_VIDEO)){
         SDL_Log("erro na inicializacao: %s", SDL_GetError());
         return 1;
     }
 
+    int windowWidth = 800;
+    int windowHeight = 400;
+
     //cria a janela
-    SDL_Window *pwindow = SDL_CreateWindow("window", 900, 600, 0);
+    SDL_Window *pwindow = SDL_CreateWindow("window", windowWidth, windowHeight, 0);
     if(!pwindow){
         SDL_Log("nao foi possivel criar a tela: %s", SDL_GetError());
         SDL_APP_FAILURE;
@@ -23,6 +27,22 @@ int main(int args, char *argv[]){
     
     bool running = true;
     SDL_Event event;
+    const char* url = "https://www.youtube.com/watch?v=OaQhEcea10k&list=RDOaQhEcea10k&start_radio=1";
+
+    //centralizando texto na tela
+    int totalChars = strlen(url);
+    float textWidth = totalChars * 8.0f;
+    float textHeight = 8.0f;
+
+    float xCentered = (windowWidth - textWidth)/ 2.0f;
+    float yCentered = (windowHeight - textHeight) / 2.0f;
+
+    SDL_FRect textArea;
+    textArea.x = xCentered;
+    textArea.y = yCentered - 4.0f;
+    textArea.w = textWidth;
+    textArea.h = 16.0f;
+
 
     //loop principal da janela
     while(running){
@@ -30,6 +50,29 @@ int main(int args, char *argv[]){
         while(SDL_PollEvent(&event)){
             if(event.type == SDL_EVENT_QUIT){
                 running = false;
+            }
+            //capturando eventos do teclado
+            else if(event.type == SDL_EVENT_KEY_DOWN){
+                if(event.key.key == SDLK_C && (event.key.mod & SDL_KMOD_CTRL)){
+                    SDL_SetClipboardText(url);
+                    SDL_Log("texto copiado");
+                }
+            }
+
+            //capturando eventos do mouse
+            else if(event.type == SDL_EVENT_MOUSE_BUTTON_DOWN){
+                //verifica se foi o botão esquerdo do mouse
+                if(event.button.button == SDL_BUTTON_LEFT){
+                    float mouseX = event.button.x;
+                    float mouseY = event.button.y;
+
+                    //verifica se a coordenada do clique está dentro da área do texto
+                    if(mouseX >= textArea.x && mouseX <= (textArea.x + textArea.w) &&
+                       mouseY >= textArea.y && mouseY <= (textArea.y + textArea.h)){
+                        SDL_SetClipboardText(url);
+                        SDL_Log("texto copiado");
+                    }
+                }
             }
         }
 
@@ -42,7 +85,8 @@ int main(int args, char *argv[]){
     
         //desenhando texto na tela
         SDL_SetRenderDrawColor(prenderer, 255, 255, 255, 255);
-        SDL_RenderDebugText(prenderer, 400, 20, "hello world!");
+        SDL_RenderDebugText(prenderer, xCentered, yCentered - 15.0f, "hello world!");
+        SDL_RenderDebugText(prenderer, xCentered, yCentered, url);
 
         //atualiza a janela com o novo frame
         SDL_RenderPresent(prenderer);
